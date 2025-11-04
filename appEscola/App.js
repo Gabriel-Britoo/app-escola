abd6cedb1dbf7994f4eec44ed4722c6f593702bd
Isadora-Gomes <isasilvacpv@gmail.com>
2025-11-04 10:09:32 -0300

    app js

--- appEscola/App.js
+++ appEscola/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import Login from './src/screens/Login';
+import TelaProf from './src/screens/TelaProf';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function TabsNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator >
+        <Tab.Screen name="Login" component={Login} />
+        <Tab.Screen name="prof" component={TelaProf} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default TabsNavigator;
